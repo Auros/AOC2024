a1686d2f2672324aeb76aed14285344a917ac5ed
Auros Nexus <41306347+Auros@users.noreply.github.com>
2024-12-01 18:25:54 -0500

    Day 1 Part 1

--- src/Day01/Program.cs
+++ src/Day01/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+Span<int> left = stackalloc int[1000];
+Span<int> right = stackalloc int[1000];
 
-Console.WriteLine("Hello, World!");
+var lines = File.ReadAllLines("Input.txt");
+for (int i = 0; i < lines.Length; i++)
+{
+    var sides = lines[i].Split("   ");
+    left[i] = int.Parse(sides[0]);
+    right[i] = int.Parse(sides[1]);
+}
+
+left.Sort();
+right.Sort();
+
+int total = 0;
+for (int i = 0; i < left.Length; i++)
+    total += Math.Abs(left[i] - right[i]);
+    
+Console.WriteLine(total);
+
+// I'll do part 2 later

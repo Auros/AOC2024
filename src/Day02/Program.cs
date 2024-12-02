15a9339d817d617a751e4c707efc8bc5cf452f0c
Auros Nexus <41306347+Auros@users.noreply.github.com>
2024-12-02 09:52:48 -0500

    Day 2 Part 1

---
+++ src/Day02/Program.cs
@@ -0,0 +1,35 @@
+﻿int safeReports = 0;
+foreach (var line in File.ReadAllLines("Input.txt"))
+{
+    Directionality? directionality = null;
+    var samples = line.Split(" ").Select(int.Parse).ToArray();
+    for (int i = 0; i < samples.Length - 1; i++)
+    {
+        var firstSample = samples[i];
+        var secondSample = samples[i + 1];
+        
+        // are numbers same
+        if (firstSample == secondSample)
+            break;
+
+        var difference = firstSample - secondSample;
+        var localizedDirectionality = difference > 0 ? Directionality.Decreasing : Directionality.Increasing;
+        directionality ??= localizedDirectionality;
+
+        // at most 3
+        if (difference is > 3 or < -3)
+            break;
+        
+        // end of report
+        if (samples.Length == i + 2)
+            safeReports++;
+    }
+}
+
+Console.WriteLine($"Part 1: {safeReports}");
+
+enum Directionality
+{
+    Increasing,
+    Decreasing
+}

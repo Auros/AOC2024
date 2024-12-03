5fbb9e06009e78092a9be2f8118030be183d37cb
Auros Nexus <41306347+Auros@users.noreply.github.com>
2024-12-03 10:32:51 -0500

    Day 3

---
+++ src/Day03/MullRegex.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day03;
+
+public partial struct MullRegex
+{
+    private const string MullRegexString = @"(mul\((?<First>([0-9]+))*,(?<Second>([0-9]+))*\))";
+    private const string MullDoDontRegexString = @"((?<Key>[a-z']+)\(\))|(mul\((?<First>([0-9]+))*,(?<Second>([0-9]+))*\))";
+
+    [GeneratedRegex(MullRegexString, RegexOptions.Multiline)]
+    public static partial Regex Mull();
+    
+    [GeneratedRegex(MullDoDontRegexString, RegexOptions.Multiline)]
+    public static partial Regex MullDoDont();
+}

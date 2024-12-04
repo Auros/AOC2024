e1a419104dc31a57733b755ab24f43f6d9bf0c14
Auros Nexus <41306347+Auros@users.noreply.github.com>
2024-12-04 12:39:47 -0500

    Day 4 Part 2

--- Benchmarks.md
+++ Benchmarks.md
@@ -1,3 +1,12 @@
+# Benchmarks
+For people that I know who are doing AOC2024 and using C#
+
+### Disclaimer
+The input is loaded **before** the benchmarks start, so the initial data input's allocation is not represented in these benchmarks.
+
+Each solution may have had slight adjustments to be compatible with being run multiple times (primarily instances of StreamReader).
+
+The benchmarks is to see how long it takes to run *both* Part 1 and Part 2. If the solution happens to calculate both part 1 and part 2 in a single pass, then the result from the single pass is used.
 ```
 === Day01 ===
 
@@ -45,4 +54,21 @@ AMD Ryzen 7 7800X3D, 1 CPU, 16 logical and 8 physical cores
 | Auros          | 484.82 us | 9.136 us | 7.133 us | 485.86 us | 15.82 | 42.9688 | 34.1797 | 2205057 B |          NA |
 | Waffle         | 273.83 us | 5.131 us | 4.799 us | 272.77 us |  8.94 | 14.6484 |  6.8359 |  740912 B |          NA |
 | AurosZeroAlloc |  31.15 us | 1.309 us | 3.861 us |  32.96 us |  1.02 |       - |       - |         - |          NA |
+
+
+
+=== Day04 ===
+
+BenchmarkDotNet v0.14.0, Windows 11 (10.0.22631.4460/23H2/2023Update/SunValley3)
+AMD Ryzen 7 7800X3D, 1 CPU, 16 logical and 8 physical cores
+.NET SDK 9.0.100
+  [Host]     : .NET 9.0.0 (9.0.24.52809), X64 RyuJIT AVX-512F+CD+BW+DQ+VL+VBMI
+  DefaultJob : .NET 9.0.0 (9.0.24.52809), X64 RyuJIT AVX-512F+CD+BW+DQ+VL+VBMI
+
+
+| Method | Mean     | Error    | StdDev   | Median   | Ratio | RatioSD | Allocated | Alloc Ratio |
+|------- |---------:|---------:|---------:|---------:|------:|--------:|----------:|------------:|
+| Auros  | 634.8 us | 12.64 us | 22.13 us | 623.5 us |  1.00 |    0.05 |         - |          NA |
+| Waffle | 126.7 us |  7.63 us | 22.37 us | 121.8 us |  0.20 |    0.04 |         - |          NA |
 ```
+
--- src/Day04/Program.cs
+++ src/Day04/Program.cs
@@ -4,6 +4,7 @@ var width = data.IndexOf('\n') - 1; // look for the first newline
 var height = data.Count('\n') + 1; // count how many newlines + 1 for the last line
 
 int xmasCount = 0;
+int xmasCountPart2 = 0;
 int incrementer = 0;
 
 // load the thingies without the newlines
@@ -38,16 +39,52 @@ directions[7] = new Vector2Int(1, 1);
 
 for (int i = 0; i < stages.Length; i++)
 {
-    if (stages[i] is not Stage.X)
-        continue;
+    if (stages[i] is Stage.X)
+    {
+        // Start looking at X and recursively scan neighbors
+        foreach (var direction in directions)
+            if (Searcher.IsValid(width, height, stages, i, Stage.X, direction))
+                xmasCount++;
+    }
+    else if (stages[i] is Stage.A)
+    {
+        var rowIndex = i % width;
+        var columnIndex = i / width;
 
-    // Start looking at X and recursively scan neighbors
-    foreach (var direction in directions)
-        if (Searcher.IsValid(width, height, stages, i, Stage.X, direction))
-            xmasCount++;
+        // no point in checking stuff on the edges
+        if (rowIndex == 0 || rowIndex == width - 1 || columnIndex == 0 || columnIndex == height - 1)
+            continue;
+        
+        // Look for the X pattern
+        int validCorners = 0;
+        
+        // top left
+        var corner = i - 1 - width;
+        if (stages[corner] is Stage.M && Searcher.IsValid(width, height, stages, corner, Stage.M, new Vector2Int(1, 1)))
+            validCorners++;
+        
+        // top right
+        corner = i + 1 - width;
+        if (stages[corner] is Stage.M && Searcher.IsValid(width, height, stages, corner, Stage.M, new Vector2Int(-1, 1)))
+            validCorners++;
+
+        // bottom left
+        corner = i - 1 + width;
+        if (stages[corner] is Stage.M && Searcher.IsValid(width, height, stages, corner, Stage.M, new Vector2Int(1, -1)))
+            validCorners++;
+        
+        // bottom right
+        corner = i + 1 + width;
+        if (stages[corner] is Stage.M && Searcher.IsValid(width, height, stages, corner, Stage.M, new Vector2Int(-1, -1)))
+            validCorners++;
+
+        if (validCorners == 2)
+            xmasCountPart2++;
+    }
 }
 
-Console.WriteLine(xmasCount);
+Console.WriteLine("Part 1: " + xmasCount);
+Console.WriteLine("Part 2: " + xmasCountPart2);
 
 internal enum Stage
 {
@@ -70,7 +107,7 @@ internal class Searcher
         var nextColumnIndex = columnIndex + direction.Y;
 
         // out of bounds
-        if (0 > nextRowIndex || nextRowIndex == width || 0 > nextColumnIndex || nextColumnIndex == height)
+        if (0 > nextRowIndex || nextRowIndex >= width || 0 > nextColumnIndex || nextColumnIndex >= height)
             return false;
     
         var nextIndex = index + direction.X + direction.Y * width;
